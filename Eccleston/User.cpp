3160fb4a8b5573c58ca3fe4cf724efc594666ace
PhantomD <klinger.steven.contact@gmail.com>
2014-11-21 11:49:05 +0100

    Steven User 21/11/2014

--- Eccleston/User.cpp
+++ Eccleston/User.cpp
@@ -4,6 +4,13 @@ class User
 {
 
 public:
+	char* getFirstName() const{ return this->firstName; }
+	char* getName() const { return this->name; }
+	char* getPassword() const{ return this->password; }
+	char* getLogin() const{ return this->login; }
+	char* getEmail() const{ return this->email; }
+
+protected :
 	char* firstName;
 	char* name;
 	char* password;
