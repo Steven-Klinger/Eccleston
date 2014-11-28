e33e72e11df7bf9e53b06202d90e220ea9148d63
Desmero <raphael.merkling@gmail.com>
2014-11-28 17:20:22 +0100

    Raphael Ajout classe Notification
    Ajout de la classe Notification:
    + Ajout de Notification.cpp
    + Ajout de Notification.h

---
+++ Eccleston/Notification.cpp
@@ -0,0 +1,34 @@
+#include "Notification.h"
+#include <iostream>
+#include <ctime>
+#include <string>
+
+Notification::Notification()
+{
+}
+
+string getSubject()
+{
+	return NULL;
+}
+
+string getMessage()
+{
+	return NULL;
+}
+
+void setSubject(string)
+{
+}
+
+void setMessage(string)
+{
+}
+
+void setDateRecive(tm)
+{
+}
+
+Notification::~Notification()
+{
+}
---
+++ Eccleston/Notification.h
@@ -0,0 +1,26 @@
+#ifndef NOTIFICATION_H
+#define NOTIFICATION_H
+
+#include <iostream>
+using namespace std;
+
+class Notification
+{
+public:
+	Notification();
+	string getSubject();
+	string getMessage();
+	tm getDateRecive();
+	void setSubject(string);
+	void setMessage(string);
+	void setDateRecive(tm);
+	~Notification();
+
+protected:
+	string subject;
+	string message;
+	struct tm dateRecive;
+};
+
+
+#endif
