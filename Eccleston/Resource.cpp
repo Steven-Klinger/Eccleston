35234ce944c8cd91232a42bf7177bfe46eea33ea
Nicolas ANDUZE <anduze.nicolas.cours@gmail.com>
2014-12-23 14:35:25 +0100

    Nicolas - Ajout classe Resource

---
+++ Eccleston/Resource.cpp
@@ -0,0 +1,9 @@
+#include "Resource.h"
+#include <iostream>
+#include <string>
+
+Resource::Resource(string name, tm dateCreation, int visible){
+	this->name = name;
+	this->dateCreation = dateCreation;
+	this->visible = visible;
+}
---
+++ Eccleston/Resource.h
@@ -0,0 +1,26 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include <iostream>
+using namespace std;
+
+class Resource{
+public:
+	Resource(string, tm, int);
+	
+	tm getDateCreation() const{ return this->dateCreation; }
+	string getName() const{ return this->name; }
+	int getTypeResource();
+	int isVisible() const{ return this->visible; }
+	void setDateCreation(tm);
+	void setName(string);
+	void setVisible(int);
+	~Resource();
+
+protected:
+	string name;
+	tm dateCreation;
+	int visible;
+};
+
+#endif
