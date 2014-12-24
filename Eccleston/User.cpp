0819bda57868a8f9529fdf17be250002e93367ec
PhantomD <klinger.steven.contact@gmail.com>
2014-12-24 16:19:45 +0100

    User
    Joyeux noel

--- Eccleston/User.cpp
+++ Eccleston/User.cpp
@@ -1,8 +1,19 @@
-#include "User.h"
-#include <iostream>
-#include <string>
+#include<iostream>
 
+class User
+{
 
+public:
+	char* getFirstName() const{ return this->firstName; }
+	char* getName() const { return this->name; }
+	char* getPassword() const{ return this->password; }
+	char* getLogin() const{ return this->login; }
+	char* getEmail() const{ return this->email; }
 
-
-
+protected :
+	char* firstName;
+	char* name;
+	char* password;
+	char* login;
+	char* email;
+};
