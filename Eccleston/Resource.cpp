a3ba034a8798e9b47b501665f089769b5937398a
Nicolas ANDUZE <anduze.nicolas.cours@gmail.com>
2014-12-29 16:43:11 +0100

    Update Resource

--- Eccleston/Resource.cpp
+++ Eccleston/Resource.cpp
@@ -8,6 +8,21 @@ Resource::Resource(string name, tm dateCreation, int visible){
 	this->visible = visible;
 }
 
+int Resource::getTypeResource(){
+	return 0;
+}
+
+void Resource::setDateCreation(tm time){
+	dateCreation = time;
+}
+
+void Resource::setName(string s){
+	name = s;
+}
+
+void Resource::setVisible(int i){
+	visible = i;
+}
 
 Resource::~Resource(){
 
