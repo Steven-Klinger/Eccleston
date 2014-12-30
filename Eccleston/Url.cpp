35a1767c0baf5874c4c5c5789ffc9e45f0ca2673
PhantomD <klinger.steven.contact@gmail.com>
2014-12-30 17:16:43 +0100

    Update Url.cpp

--- Eccleston/Url.cpp
+++ Eccleston/Url.cpp
@@ -2,8 +2,9 @@
 #include "Url.h"
 #include <string>
 
-Url::Url(string name, int visible, string path) : File(string name, int visible, string path){
+Url::Url(string name, int visible, string link) : Resource(string name, tm dateCreation, int visible){
 	this->url = url;
+	this->link = link; //on l'appelle link le lien non?
 }
 
 int Url::getTypeResource(){
