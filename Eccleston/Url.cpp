9be7c562fa9af020c43f8bd328b1f16b14025a5a
PhantomD <klinger.steven.contact@gmail.com>
2014-12-30 17:17:29 +0100

    Url.cpp à checker
     (j'ai un doute sur le dernier string)

--- Eccleston/Url.cpp
+++ Eccleston/Url.cpp
@@ -4,7 +4,7 @@
 
 Url::Url(string name, int visible, string link) : Resource(string name, tm dateCreation, int visible){
 	this->url = url;
-	this->link = link; //on l'appelle link le lien non?
+	this->link = link; //on l'appelle link le lien non??
 }
 
 int Url::getTypeResource(){
