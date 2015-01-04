8d0b5db8dd09db55d74a77dfdecb78423ef5a21e
Raphaël <raphael.Merkling@gmail.com>
2015-01-04 21:02:36 +0100

    Update Resource.h

--- Eccleston/Resource.h
+++ Eccleston/Resource.h
@@ -1,46 +1,40 @@
 //{{NO_DEPENDENCIES}}
-// Fichier Include généré de Microsoft Visual C++.
-// Utilisé par projResource.rc
+// Fichier Include gÃ©nÃ©rÃ© de Microsoft Visual C++.
+// UtilisÃ© par projResource.rc
 //
 #define IDD_CONNEXION                   101
 #define IDD_MAIN_ADMIN                  111
-#define IDD_LESSON_ADMIN                112
+#define IDD_STUDENT						112
 #define IDD_ADDUSER                     113
 #define IDD_MAIN_TEACHER                121
 #define IDD_LESSON_TEACHER              122
 #define IDD_MAIN_STUDENT                131
 #define IDD_LESSON_STUDENT              132
-
-// Connexion Window
 #define IDC_TEXT_LOGIN                  1001
 #define IDC_TEXT_PASSW                  1002
 #define IDC_CONNECT                     1003
-
-// Common Window
 #define IDC_DECONNECT                   1004
 #define IDC_EXIT                        1005
 #define IDC_STATIC_USERNAME             1006
-
-// Main Window
 #define IDC_LIST_LESSON                 1007
 #define IDC_LESSON                      1008
-
-// Lesson Window
 #define IDC_LIST_RESSOURCES             1009
 #define IDC_MAIN                        1010
-
-// Admin Main Window
 #define IDC_ADDUSER                     1101
-
-// Admin AddUser Window
 #define IDC_ADDUSER_VALIDATE            1102
 #define IDC_EDIT_NAME                   1103
 #define IDC_EDIT_FIRSTNAME              1104
 #define IDC_EDIT_EMAIL                  1105
 #define IDC_EDIT_LOGIN                  1106
 #define IDC_EDIT_PASSWORD               1107
-#define IDC_COMBO_USERTYPE              1108
-
+#define IDC_RADIO_ADMIN                 1108
+#define IDC_RADIO_TEACH                 1109
+#define IDC_RADIO_STUD                  1110
+#define IDC_USER						1111
+#define IDC_LIST_ADMIN					1112
+#define IDC_LIST_TEACHER				1113
+#define IDC_LIST_STUDENT				1114
+#define IDC_REMOVE_USER					1115
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -49,7 +43,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_CONTROL_VALUE         1021
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
